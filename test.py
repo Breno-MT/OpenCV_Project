e3e1b17e4feb9b6fbc86d976ab8718c547a7a861
Breno-MT <hentgenp@gmail.com>
2024-06-18 11:47:41 -0300

    Revert "FEAT: added test.py to verify if OpenCV is working properly"
    This reverts commit 9a15d98726afaaf4d918a7d35f189e587ce92d30.

--- test.py
+++
@@ -1,5 +0,0 @@
-import cv2 as cv
-img = cv.imread("/home/scr0ll/Pictures/artworks-bxRk7aOfocMK2hVS-s5ITNA-t500x500.jpg")
-
-cv.imshow("Display window", img)
-k = cv.waitKey(0) # Wait for a keystroke in the window
