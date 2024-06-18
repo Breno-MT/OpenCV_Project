460b976a083bb781e6dfe63c6bcc2229cc8ff33d
Breno-MT <hentgenp@gmail.com>
2024-06-18 12:12:50 -0300

    REFACT: added comments about frame, added prints about width/height of webcam frames

--- webcam_visualizer.py
+++ webcam_visualizer.py
@@ -9,12 +9,16 @@ if not cap.isOpened():
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
+
+    # You can set width and height to your prefence using the code below
+    # ret = cap.set(cv.CAP_PROP_FRAME_WIDTH, 320)
+    # ret = cap.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
  
     # if frame is read correctly ret is True
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    
+
     # Our operations on the frame come here
     # color = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     color = cv.cvtColor(frame, cv.COLOR_BGR2BGRA)
@@ -22,6 +26,11 @@ while True:
     cv.imshow('frame', color)
     if cv.waitKey(1) == ord('q'):
         break
+
+
+# Print some informations about WIDTH and HEIGHT
+print(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
 
 # When everything done, release the capture
 cap.release()
